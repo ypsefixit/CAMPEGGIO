05945e3c3355d3d2c5a189e381279d2f8cd4846f
ypsefixit <velvetjam@gmail.com>
2025-04-29 05:20:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let databaserisorse = [];
 
-function getFormattedDate() {
-  const today = new Date();
-  const day = String(today.getDate()).padStart(2, '0');
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const year = String(today.getFullYear()).slice(-2); // Anno a 2 cifre
-  return `${day}/${month}/${year}`;
-}
+
 
 function getTodayDate() {
   const today = new Date();
